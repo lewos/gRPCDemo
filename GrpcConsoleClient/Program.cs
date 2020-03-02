c87b2b94dbe001e6a8ab9eb15ef1f98b11db9db2
Leo <leo.21@live.com.ar>
2020-03-02 00:35:09 -0300

    first commit

---
+++ GrpcConsoleClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //// configuracion del canal, escuchando al servidor de grpc
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var input = new HelloRequest { Name="Leo" };
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            //-----------------------------------------------------
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Customer.CustomerClient(channel);
+
+            var input = new CustomerLookupModel { UserId = 2 };
+
+            var reply = await client.GetCustomerInfoAsync(input);
+
+            Console.WriteLine($"{reply.FirstName}, {reply.LastName}");
+            Console.WriteLine("---------------");
+
+            //-----------------------------------------------------
+            using (var call = client.GetNewCustomers(new NewCustomerRequest())) 
+            {
+                while (await call.ResponseStream.MoveNext()) 
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName}");
+
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcService/Services/CustomersService.cs
@@ -0,0 +1,82 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcService.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "firstName1";
+                output.LastName = "lastName1";
+
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "firstName2";
+                output.LastName = "lastName2";
+            }
+            else 
+            {
+                output.FirstName = "firstNameOther";
+                output.LastName = "lastNameOther";
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerRequest request, 
+            IServerStreamWriter<CustomerModel> responseStream, 
+            ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    Age = 5,
+                    EmailAdress = "algo@gmail.com",
+                    FirstName = "leo",
+                    IsAlive = true,
+                    LastName = "carmi"
+                },
+                new CustomerModel
+                {
+                    Age = 4,
+                    EmailAdress = "grpc@gmail.com",
+                    FirstName = "nico",
+                    IsAlive = true,
+                    LastName = "carmi"
+                },
+                new CustomerModel
+                {
+                    Age = 7,
+                    EmailAdress = "creo@gmail.com",
+                    FirstName = "jose",
+                    IsAlive = true,
+                    LastName = "carmi"
+                }
+            };
+
+            foreach(var c in customers) 
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(c);
+            }
+        }
+    }
+}
